4efc813d0c96d17840544198dff2fde8b65a7f78
Lokesh-DataScience <lokeshkumar.engineer@gmail.com>
2024-08-07 20:59:53 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,95 @@
+import streamlit as st
+from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_huggingface import HuggingFaceEmbeddings
+# from InstructorEmbedding import INSTRUCTOR
+from langchain_community.vectorstores import FAISS
+from langchain.memory.buffer import ConversationBufferMemory
+from langchain.chains.conversational_retrieval.base import ConversationalRetrievalChain
+from langchain_community.chat_models import ChatOllama
+from htmlTemplates import css,bot_template,user_template
+
+def get_doc_text(documents):
+    text = ""
+    for doc in documents:
+        doc_reader = PdfReader(doc)
+        for page in doc_reader.pages:
+            text += page.extract_text()
+    return text
+
+def get_text_chunks(text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len,
+        is_separator_regex=False,
+    )
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+def get_vectorstore(text_chunks):
+    model_name = "sentence-transformers/all-mpnet-base-v2"
+    model_kwargs = {'device': 'cpu'}
+    encode_kwargs = {'normalize_embeddings': False}
+    hf = HuggingFaceEmbeddings(
+        model_name=model_name,
+        model_kwargs=model_kwargs,
+        encode_kwargs=encode_kwargs
+    )
+    vectorstore = FAISS.from_texts(text_chunks, hf)
+    return vectorstore
+
+def get_conversation_chain(vectorstore):
+    memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
+    llm = ChatOllama(
+        model="llama3",
+        temperature=0
+    )
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=vectorstore.as_retriever(),
+        memory=memory
+    )
+    return conversation_chain
+
+def handle_userinput(user_question):
+    response = st.session_state.conversation({'question': user_question})
+    st.session_state.chat_history = response['chat_history']
+    for i, message in enumerate(st.session_state.chat_history):
+        if i % 2 ==0:
+            st.write(user_template.replace("{{MSG}}",message.content),unsafe_allow_html=True)
+        else:
+            st.write(bot_template.replace("{{MSG}}",message.content),unsafe_allow_html=True)
+
+
+def main():
+    load_dotenv()
+    st.set_page_config(page_title="Chat-DOC", page_icon=":books:")
+
+    st.write(css, unsafe_allow_html=True)
+    
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = None
+    st.header("Chat with Documents :books:")
+
+    user_question = st.text_input("Ask a question about your documents:")
+    if st.button("Submit"):
+        if st.session_state.conversation:
+            handle_userinput(user_question)
+
+    with st.sidebar:
+        st.subheader("Your Documents")
+        docs = st.file_uploader("Upload your documents and click on 'process'", accept_multiple_files=True)
+        if st.button("Process"):
+            with st.spinner("Processing"):
+                raw_text = get_doc_text(docs)
+                text_chunks = get_text_chunks(raw_text)
+                vector_store = get_vectorstore(text_chunks)
+                st.session_state.conversation = get_conversation_chain(vector_store)
+
+if __name__ == "__main__":
+    main()
