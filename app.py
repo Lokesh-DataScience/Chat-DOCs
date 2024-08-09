55365eae7e45ce302a0acfb7fec2bd2f5a6c3fcd
Lokesh-DataScience <lokeshkumar.engineer@gmail.com>
2024-08-09 14:04:00 +0530

    Initial commit

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain.memory.buffer import ConversationBufferMemory
 from langchain.chains.conversational_retrieval.base import ConversationalRetrievalChain
+from langchain_community.chat_models import ChatOllama
 from htmlTemplates import css,bot_template,user_template
-from langchain_huggingface import ChatHuggingFace, HuggingFacePipeline
 
 def get_doc_text(documents):
     text = ""
@@ -42,19 +42,11 @@ def get_vectorstore(text_chunks):
     return vectorstore
 
 def get_conversation_chain(vectorstore):
-
-    llm = HuggingFacePipeline.from_model_id(
-        model_id="HuggingFaceH4/zephyr-7b-beta",
-        task="text-generation",
-        pipeline_kwargs=dict(
-            max_new_tokens=512,
-            do_sample=False,
-            repetition_penalty=1.03,
-        )
-    )
-
-
     memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
+    llm = ChatOllama(
+        model="llama3",
+        temperature=0
+    )
     conversation_chain = ConversationalRetrievalChain.from_llm(
         llm=llm,
         retriever=vectorstore.as_retriever(),
