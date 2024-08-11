aada5c9a5b3e6b040077e8fe003ba9efac4a56b7
Lokesh-DataScience <lokeshkumar.engineer@gmail.com>
2024-08-11 19:44:19 +0530

    Initial commit

--- htmlTemplates.py
+++ htmlTemplates.py
@@ -37,8 +37,9 @@ bot_template = '''
 user_template = '''
 <div class="chat-message user">
     <div class="avatar">
-        <img src="https://i.ibb.co/rdZC7LZ/Photo-logo-1.png">
+        <img src="https://i.ibb.co/qs0qtJt/Untitled-design.png">
     </div>    
     <div class="message">{{MSG}}</div>
 </div>
 '''
+
